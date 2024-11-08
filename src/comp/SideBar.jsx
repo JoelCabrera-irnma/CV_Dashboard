c8c38813c83b3ca385682681c6ebe09748f1a731
Joel Cabrera <140646912+JoelCabrera-irnma@users.noreply.github.com>
2024-11-08 17:29:59 -0300

    Inicio de proyecto CV-Dashboard a partir de plantilla propia

---
+++ src/comp/SideBar.jsx
@@ -0,0 +1,95 @@
+
+const Sidebar = ({ handleButtonClick }) => {
+  return (
+    <div className="sidebar">
+      <div className="sideTop">
+        <a href="#">
+          <img
+            src="src/assets/icons/Sidebar/dashboard_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="dash"
+          />
+          DashBoard
+        </a>
+      </div>
+      <div className="sideElements1">
+        <a href="#" onClick={() => handleButtonClick(0)}>
+          <img
+            src="src/assets/icons/Sidebar/home_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico1"
+          />
+          Home
+        </a>
+        <a href="#" onClick={() => handleButtonClick(1)}>
+          <img
+            src="src/assets/icons/Sidebar/person_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico2"
+          />
+          Profile
+        </a>
+        <a href="#" onClick={() => handleButtonClick(2)}>
+          <img
+            src="src/assets/icons/Sidebar/mode_comment_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico3"
+          />
+          Projects
+        </a>
+        <a href="#" onClick={() => handleButtonClick(3)}>
+          <img
+            src="src/assets/icons/Sidebar/history_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico4"
+          />
+          History
+        </a>
+        <a href="#" onClick={() => handleButtonClick(4)}>
+          <img
+            src="src/assets/icons/Sidebar/task_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico5"
+          />
+          Tasks
+        </a>
+        <a href="#" onClick={() => handleButtonClick(5)}>
+          <img
+            src="src/assets/icons/Sidebar/groups_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="ico6"
+          />
+          Communities
+        </a>
+      </div>
+      <div className="sideElements2">
+        <a href="#">
+          <img
+            src="src/assets/icons/Sidebar/settings_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="icoBottom1"
+          />
+          Setting
+        </a>
+        <a href="#">
+          <img
+            src="src/assets/icons/Sidebar/help_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="icoBottom2"
+          />
+          Support
+        </a>
+        <a href="#">
+          <img
+            src="src/assets/icons/Sidebar/verified_user_FILL0_wght400_GRAD0_opsz24.svg"
+            alt=""
+            className="icoBottom3"
+          />
+          Privacy
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/scripts/utils.jsx
@@ -0,0 +1,33 @@
+import ListSquares from '../comp/mapSquare.jsx'
+import Profile from '../comp/Profile.jsx'
+ 
+// Renderizado condicional del componente en Main
+export const renderComponent = (count) => {
+    switch (count) {
+      case 0: 
+        return <div>Hola aqui va la carta de presentacion</div>
+      case 1:
+        return <Profile/>;
+      case 2:
+        return <ListSquares/>
+      default:
+        return 
+    }
+  };
+
+  export const replaceClassInMain = (component) => {
+    const mainElement = document.querySelector('#main__content');
+    
+    if (mainElement) {
+      // Remueve cualquier clase existente que no coincida con el nuevo componente
+      mainElement.classList.remove(...mainElement.classList)
+      if(component==0){
+        mainElement.classList.add('projects');
+      }
+      if(component==1){
+        mainElement.classList.add('profile');
+      }
+      // Agrega la nueva clase basada en el componente
+      
+    }
+  };
