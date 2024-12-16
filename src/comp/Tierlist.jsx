96d09bfbec8bfbc42b6dd9e987a3c6cf83e9fc48
Joel Cabrera <140646912+JoelCabrera-irnma@users.noreply.github.com>
2024-12-16 16:46:48 -0300

    Fix bug with deploy in Vercel

--- src/comp/Tierlist.jsx
+++ src/comp/Tierlist.jsx
@@ -1,15 +1,10 @@
-import { useState, useEffect } from 'react';
-import { DndContext } from '@dnd-kit/core';
-import { Draggable } from './Draggable.jsx';
-import { Droppable } from './Dropable.jsx';
-
+import { useState, useEffect } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { Draggable } from "./Draggable.jsx";
+import { Droppable } from "./Dropable.jsx";
+import "./styles/TierList.css";
 
 const ICON_SIZE = 40;
-
-const sty = {
-  border: "1px solid red",
-  height: "200px"
-}
 
 const DragDropContainer = () => {
   const [droppedItems, setDroppedItems] = useState(new Set());
@@ -23,15 +18,15 @@ const DragDropContainer = () => {
   useEffect(() => {
     const loadIcons = async () => {
       try {
-        const response = await fetch('/src/iconList.json');
+        const response = await fetch("/src/iconList.json");
         if (!response.ok) {
-          throw new Error('Error al cargar los iconos');
+          throw new Error("Error al cargar los iconos");
         }
         const data = await response.json();
         setIcons(data);
       } catch (err) {
         setError(err.message);
-        console.error('Error al cargar iconList.json:', err);
+        console.error("Error al cargar iconList.json:", err);
       } finally {
         setIsLoading(false);
       }
@@ -40,39 +35,38 @@ const DragDropContainer = () => {
     loadIcons();
   }, []);
 
-  console.log(icons);
-  console.log(droppedItems);
-
+  //console.log(icons);
+  //console.log(droppedItems);
 
   const handleDragEnd = ({ over, active }) => {
     if (!over) return;
-    console.log(over.id)
-    console.log(active.id)
-    if(over.id=="droppable"){
-      setDroppedItems(prev => {
+    console.log(over.id);
+    console.log(active.id);
+    if (over.id == "droppable") {
+      setDroppedItems((prev) => {
         const newItems = new Set(prev);
         newItems.add(active.id);
         return newItems;
       });
     }
-    if(over.id=="dropable2"){
-      setDroppedItems2(prev => {
+    if (over.id == "dropable2") {
+      setDroppedItems2((prev) => {
         const newItems = new Set(prev);
         newItems.add(active.id);
         return newItems;
       });
     }
-    if(over.id=="dropable3"){
-      setDroppedItems3(prev => {
+    if (over.id == "dropable3") {
+      setDroppedItems3((prev) => {
         const newItems = new Set(prev);
         newItems.add(active.id);
         return newItems;
       });
     }
-    let newIcons = [...icons]
+    let newIcons = [...icons];
     const i = newIcons.indexOf(active.id);
     newIcons.splice(i, 1);
-    setIcons(newIcons)
+    setIcons(newIcons);
   };
 
   const renderDraggableItem = (iconName, index) => (
@@ -90,48 +84,45 @@ const DragDropContainer = () => {
   if (error) return <div>Error: {error}</div>;
 
   return (
-    <DndContext onDragEnd={handleDragEnd}>
-      <div className="source-container" style={sty}>
-        {icons.map((iconName, index) => (
-          renderDraggableItem(iconName, index)
-        ))}
-      </div>
+    <div className="tierList">
+      <DndContext onDragEnd={handleDragEnd}>
+        <div className="source-container">
+          {icons.map((iconName, index) => renderDraggableItem(iconName, index))}
+        </div>
 
-      <Droppable id="droppable">
-        <div className='text-tier'>S</div>
-        {droppedItems.size === 0 ? (
-          <p>Arrastra elementos aquí</p>
-        ) : (
-          Array.from(droppedItems).map((itemId, index) => {
-            return renderDraggableItem(itemId, index);
-          })
-        )}
-      </Droppable>
-      <Droppable id="dropable2">
-        <div className='text-tier'>A</div>
-        {droppedItems2.size === 0 ? (
+        <Droppable id="droppable">
+          <div className="text-tier">S</div>
+          {droppedItems.size === 0 ? (
+            <p>Arrastra elementos aquí</p>
+          ) : (
+            Array.from(droppedItems).map((itemId, index) => {
+              return renderDraggableItem(itemId, index);
+            })
+          )}
+        </Droppable>
+        <Droppable id="dropable2">
+          <div className="text-tier">A</div>
+          {droppedItems2.size === 0 ? (
             <p>Aqui suelta</p>
           ) : (
-            Array.from(droppedItems).map((itemId, index) => {
+            Array.from(droppedItems2).map((itemId, index) => {
               return renderDraggableItem(itemId, index);
             })
           )}
-      </Droppable>
-      <Droppable id="dropable3">
-        <div className='text-tier'>B</div>
-        {droppedItems3.size === 0 ? (
+        </Droppable>
+        <Droppable id="dropable3">
+          <div className="text-tier">B</div>
+          {droppedItems3.size === 0 ? (
             <p>Pull here!!!</p>
           ) : (
-            Array.from(droppedItems).map((itemId, index) => {
+            Array.from(droppedItems3).map((itemId, index) => {
               return renderDraggableItem(itemId, index);
             })
           )}
-      </Droppable>
-    </DndContext>
+        </Droppable>
+      </DndContext>
+    </div>
   );
 };
 
 export default DragDropContainer;
-
-
-
